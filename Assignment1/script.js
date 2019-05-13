82bf734c3aa5f7d534e25b3c5e74f4fec96c8b96
Tony Chen <tony.chen@outlook.com>
2019-05-12 23:06:33 -0700

    Assignment 1 Implemented

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,22 +1,90 @@
-function setSize() {
-  var size = 0;
-  var randomNum = Math.floor(Math.random() *10);
-  for (var i = 0; i < randomNum; i++) {
-    size += 100;
-  }
-  return size;
+var messages = '[{"id": 1, "alias": "Steve", "message": "I love CPSC 310"}, {"id": 2, "alias": "Danya", "message": "I love HTML"}]'
+var maxId = 3
+
+function initializeHTML() {
+  messages = JSON.parse(messages);
+  renderList();
 }
 
-function moveHorizontal() {
-  var steven = document.getElementById('picture');
-  if (steven) {
-    steven.style.left = setSize() + 'px';
+function renderList() {
+  let list = document.getElementById("messages-list");
+  list.innerHTML = "";
+  for (var x = 0; x < messages.length; x++) {
+    let newElement = createElement(messages[x].message, messages[x].alias, messages[x].id);
+    list.appendChild(newElement);
   }
 }
 
-function moveVertical() {
-  var steven = document.getElementById('picture');
-  if (steven) {
-    steven.style.top = setSize() + 'px';
+function createElement(message, alias, id) {
+  let newElement = document.createElement("li");
+  if (alias == "") {
+    alias = "Anonymous";
   }
+  newElement.innerHTML = message + " - " + alias + " ";
+  let button = document.createElement("button");
+  button.innerHTML = "x";
+  button.setAttribute("id", id);
+  button.setAttribute("onClick", "removeMessage(this.id)");
+  newElement.appendChild(button);
+  return newElement;
+}
+
+function submitMessage() {
+  let message = document.getElementById("message").value;
+  if (message.length == 0) {
+    alert("You must enter a message!");
+    return;
+  }
+  let alias = document.getElementById("alias").value;
+  if (alias == "") {
+    alias = "Anonymous";
+  }
+  let list = document.getElementById("messages-list");
+  let newElement = createElement(message, alias, maxId);
+  let jsonElement = {
+    "id": maxId,
+    "message": message,
+    "alias": alias
+  }
+  maxId++;
+  messages.push(jsonElement);
+  list.appendChild(newElement);
+}
+
+function clearMessage() {
+  let message = document.getElementById("message");
+  let alias = document.getElementById("alias");
+  message.value = "";
+  alias.value = "";
+}
+
+function printMessage() {
+  console.log(JSON.stringify(messages));
+}
+
+
+function deleteMessage() {
+  if (window.prompt("This will cause immense destruction. Remove? Type 'Yes' to confirm",
+  "No") != "Yes") {
+    alert("Operation cancelled");
+    return;
+  }
+  messages = [];
+  renderList();
+}
+
+function removeMessage(id) {
+  for (var x = 0; x < messages.length; x++) {
+    if (messages[x].id == id) {
+      if (!confirm("Deleting element: " + messages[x].message
+      + " by " + messages[x].alias)) {
+        alert("Operation cancelled");
+        return;
+      }
+      messages.splice(x, 1);
+      break;
+    }
+  }
+  renderList();
+  alert("Successfully deleted");
 }
